f4cf299a806a3e1fe9f4c3ebabb09ffabbd32a0e
jeonsumin <soomis7@naver.com>
2020-01-03 20:18:23 +0900

    메모편집

--- TRMemo/TRmemo/TRmemo/ComposeViewController.swift
+++ TRMemo/TRmemo/TRmemo/ComposeViewController.swift
@@ -9,41 +9,66 @@
 import UIKit
 
 class ComposeViewController: UIViewController {
-
+    
+    var editTarget: Memo?
+    var originalMemoContnet: String?
+    
     @IBOutlet var memoTextView: UITextView!
     
     //dismiss > 모달창을 닫을때 사용
     @IBAction func close(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-
+    
     @IBAction func save(_ sender: Any) {
         let memo = memoTextView.text
         
-//        let newMemo = Memo(content: memo ?? "")
-//        Memo.dummyMemoList.append(newMemo)
-        DataManager.shared.addNewMemo(memo)
-//
+        //        let newMemo = Memo(content: memo ?? "")
+        //        Memo.dummyMemoList.append(newMemo)
+        
+        if let target = editTarget{
+            target.content = memo
+            DataManager.shared.saveContext()
+            NotificationCenter.default.post(name: ComposeViewController.memoDidChange, object: nil)
+        }else{
+            DataManager.shared.addNewMemo(memo)
+            NotificationCenter.default.post(name: ComposeViewController.newMemoDidInsert, object: nil)
+            
+        }
         dismiss(animated: true, completion: nil)
     }
     
     
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        if let memo = editTarget{
+            navigationItem.title = "메모편집"
+            memoTextView.text = memo.content
+            originalMemoContnet = memo.content
+        }else{
+            navigationItem.title = "새 메모"
+            memoTextView.text = ""
+        }
     }
     
-
     /*
-    // MARK: - Navigation
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
+extension ComposeViewController {
+    static let newMemoDidInsert = Notification.Name(rawValue: "newMemoDidInsert")
+    static let memoDidChange = Notification.Name(rawValue: "memoDidChange")
 }
