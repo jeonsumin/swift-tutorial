c71f5ebc475e8f4eac6fb77cdb4123fa769dd067
jeonsumin <soomis7@naver.com>
2020-01-12 18:55:39 +0900

    새 프로젝트 생성

---
+++ TRMusicPlayer/TRMusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TRMusicPlayer
+//
+//  Created by Sumin Jeon on 2020/01/12.
+//  Copyright © 2020 Sumin Jeon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
