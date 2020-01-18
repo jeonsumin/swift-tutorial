3a1537bfcaec958fd742292700c9e2a83f47c356
jeonsumin <soomis7@naver.com>
2020-01-18 17:02:07 +0900

    view의 상태변화 감지

--- VIewTransition/VIewTransition/SecondViewController.swift
+++ VIewTransition/VIewTransition/SecondViewController.swift
@@ -12,10 +12,37 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print("SecondViewController의 view 가 메로리에 로드됨")
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("SecondViewController의 view가 화면에서 보여질 예정")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("SecondViewController의 View가 화면에 보여짐")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("SecondviewController의 view가 화면에서 사라질 예정")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("SecondviewController의 view가 화면에서 사라짐")
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("SecondviewController의 view가 subview를 레이아웃 하려함")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print("SecondviewController의 view가 subview를 레이아웃 함")
+    }
     //정보의 흐름을 가지고 있을때 push and pop 을 사용
     @IBAction func popToPrev(){
         self.navigationController?.popViewController(animated: true)
--- VIewTransition/VIewTransition/ViewController.swift
+++ VIewTransition/VIewTransition/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //프로젝트 생성시 기본적으로 설정되어있는 메소드
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("ViewController의 view 가 메로리에 로드됨")
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("ViewController의 view가 화면에서 보여질 예정")
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("ViewController의 View가 화면에 보여짐")
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("viewController의 view가 화면에서 사라질 예정")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("viewController의 view가 화면에서 사라짐")
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        print("viewController의 view가 subview를 레이아웃 하려함")
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        print("viewController의 view가 subview를 레이아웃 함")
+    }
 }
 
