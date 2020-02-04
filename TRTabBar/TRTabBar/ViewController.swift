e899dabf690d1a874634bfce28b055ccea89e549
jeonsumin <soomis7@naver.com>
2020-02-04 21:52:06 +0900

    add TabBarView

---
+++ TRTabBar/TRTabBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TRTabBar
+//
+//  Created by iMac on 2020. 2. 4..
+//  Copyright © 2020년 iMac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
