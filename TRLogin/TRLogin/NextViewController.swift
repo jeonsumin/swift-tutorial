1201a01551faf97782d602bbb18367124c44f645
jeonsumin <soomis7@naver.com>
2020-03-01 11:47:30 +0900

    add Segue Project

---
+++ TRLogin/TRLogin/NextViewController.swift
@@ -0,0 +1,32 @@
+//
+//  NextViewController.swift
+//  TRLogin
+//
+//  Created by Terry on 2020/03/01.
+//  Copyright © 2020 Terry. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TRLogin/TRLogin/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  TRLogin
+//
+//  Created by Terry on 2020/03/01.
+//  Copyright © 2020 Terry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var id: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var loginbutton: UIButton!
+    
+    
+    @IBAction func login(_ sender: Any) {
+        if(password.text == "1234"){
+            performSegue(withIdentifier: "login", sender: nil)
+        }else{
+            let alert = UIAlertController(title: "아이디 확인", message: "비밀번호가 맞지 않습니다.", preferredStyle: UIAlertController.Style.alert)
+            
+            alert.addAction(UIAlertAction(title: "확인", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
