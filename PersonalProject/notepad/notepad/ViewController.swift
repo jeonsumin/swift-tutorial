bf95c26cf1031f7a50ca6135d45df64a1aebb25d
jeonsumin <soomis7@naver.com>
2020-03-03 20:10:02 +0900

    add personal project for portfolo

---
+++ PersonalProject/notepad/notepad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  notepad
+//
+//  Created by Terry on 2020/03/03.
+//  Copyright © 2020 Terry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
